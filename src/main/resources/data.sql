f0c2a68d98aaf627bb335321695f402759e92920
Sun Yifan <yifansun0125@gmail.com>
2021-05-25 16:16:19 +0900

    change date

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,12 +7,18 @@ INSERT INTO weekday VALUES(6, '土曜');
 INSERT INTO weekday VALUES(7, '日曜');
 
 
-INSERT INTO bargain VALUES(101, '牛乳半額', '肉', 1);
+INSERT INTO bargain VALUES(101, '牛肉半額', '肉', 1);
 INSERT INTO bargain VALUES(102, '牛肉半額', '肉',2);
-INSERT INTO bargain VALUES(103, '会計8%割引','会計', 3);
+INSERT INTO bargain VALUES(103, '会計8%割引','会計',2);
 INSERT INTO bargain VALUES(104, 'イチゴ半額', '果物', 2);
 INSERT INTO bargain VALUES(105, '冷凍食品半額', '惣菜', 1);
 INSERT INTO bargain VALUES(106, '30円クーポン', '会計', 7);
-INSERT INTO bargain VALUES(107, '牛肉半額', '肉',3);
+INSERT INTO bargain VALUES(107, '牛丼半額', '惣菜',3);
 INSERT INTO bargain VALUES(108, '限定商品', '惣菜',5);
 INSERT INTO bargain VALUES(109, '100円たまご', '惣菜',6);
+INSERT INTO bargain VALUES(110, '中華料理半額', '惣菜',4);
+INSERT INTO bargain VALUES(111, '牛丼半額', '惣菜',7);
+INSERT INTO bargain VALUES(112, 'とりむね20%off', '肉',5);
+INSERT INTO bargain VALUES(113, '豚肉ブロック半額', '肉',3);
+
+
